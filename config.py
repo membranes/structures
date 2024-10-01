b859d8043009d08117921115fc674459211ec15f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-01 15:15:07 +0100

    Add config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+"""config.py"""
+import os
+
+
+class Config:
+    """
+    Config
+    """
+
+    def __init__(self) -> None:
+        """
+        Constructor
+        """
+
+        self.datapath = os.path.join(os.getcwd(), 'data')
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+
+        # Addressing category imbalance ...
+        self.minimum_category_frequency = 1000
+
+        # A template of Amazon S3 (Simple Storage Service) parameters
+        self.s3_parameters_template = 'https://raw.githubusercontent.com/membranes/.github/refs/heads/master/profile/s3_parameters.yaml'
