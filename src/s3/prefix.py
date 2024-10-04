4be65b8f451f8fb4e68a53a01bccb57295627a62
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-05 00:20:10 +0100

    Update src/s3/prefix.py

--- src/s3/prefix.py
+++ src/s3/prefix.py
@@ -1,5 +1,4 @@
 """Module prefix.py"""
-import logging
 
 import botocore.exceptions
 
@@ -22,12 +21,6 @@ class Prefix:
         self.__service: sr.Service = service
         self.__s3_client = self.__service.s3_client
         self.__bucket_name = bucket_name
-
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
 
     def delete(self, objects: list[dict]):
         """
