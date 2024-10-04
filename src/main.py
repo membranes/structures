ab1c03061123d68d8f3f58a95caf7d4c0c34521f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-04 18:43:46 +0100

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -18,6 +18,8 @@ def main() -> None:
     setup = src.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
     logger.info(setup)
 
+    src.data.interface.Interface(service=service, s3_parameters=s3_parameters).exc()
+
     # Get
     # if setup:
     #     messages = src.data.interface.Interface(service=service, s3_parameters=s3_parameters).exc()
