796e24e08e2ad6473a4c4aee0cd14be6e20a7ec8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-04 23:45:04 +0100

    Update src/data/filtering.py

--- src/data/filtering.py
+++ src/data/filtering.py
@@ -18,17 +18,28 @@ class Filtering:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def __call__(self, data: pd.DataFrame, elements: pd.DataFrame) -> pd.DataFrame:
+    def __call__(self, data: pd.DataFrame, tags: pd.DataFrame) -> pd.DataFrame:
         """
 
         :param data:
-        :param elements:
+        :param tags:
         :return:
         """
 
-        frame: pd.DataFrame = data.copy().loc[data['category'].isin(values=elements['category'].unique()), :]
+        # Determining the instances/words that have viable tag categories
+        conditionals: pd.Series = data['category'].isin(values=tags['category'].unique())
+
+        # Hence, the sentences that include one or more inviable categories
+        invalid = data.copy().loc[~conditionals, 'sentence_identifier'].unique()
+
+        # Hence, valid sentence identifiers
+        markings = ~data['sentence_identifier'].isin(invalid)
+
+        # Finally, the viable instances
+        frame: pd.DataFrame = data.copy().loc[markings, :]
 
         self.__logger.info(frame.head())
+        self.__logger.info(frame['category'].unique())
         frame.info()
 
         return frame
