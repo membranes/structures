39625d33e92b3c5ceae92c2d2c029640d81efff0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-07 19:47:11 +0000

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config:
         self.minimum_category_frequency = 1000
 
         # A template of Amazon S3 (Simple Storage Service) parameters
-        self.s3_parameters_template = 'https://raw.githubusercontent.com/membranes/.github/refs/heads/master/profile/s3_parameters.yaml'
+        self.s3_parameters_template = 'https://raw.githubusercontent.com/membranes/configurations/refs/heads/master/data/s3_parameters.yaml'
 
         # Node: The raw data to be structured
         self.node = 'raw/dataset.csv'
